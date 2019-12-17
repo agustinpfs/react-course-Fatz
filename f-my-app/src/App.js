64de813c98537e061d7b5749f35a31b4af4bccf2
Agustin Palmieri <agujex@Agustins-MBP.utopia.net>
2019-12-17 12:48:09 -0300

    first render tasks

--- f-my-app/src/App.js
+++ f-my-app/src/App.js
@@ -1,5 +1,21 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
+import tasks from './sample/tasks.json';
+
+class App extends Component{
+
+  state = {
+    tasks: tasks
+  }
+
+  render() {
+    return <div>
+      {this.state.tasks.map(e => <p key={e.id}>
+        {e.title} - {e.description} - {e.done} - {e.id}        
+        </p>)}
+    </div>
+  }
+}
 
 export default App;
