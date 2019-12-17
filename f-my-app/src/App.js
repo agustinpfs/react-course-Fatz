c8570d9aeff15432e97e8995a985e6c42924f7f7
Agustin Palmieri <agujex@Agustins-MBP.utopia.net>
2019-12-17 10:54:39 -0300

    state use

--- f-my-app/src/App.js
+++ f-my-app/src/App.js
@@ -2,13 +2,45 @@ import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
 
-function HelloWorld(props) {
-  return (
-    <div id="hello">
-      <h3>{props.myText}</h3>
-      {props.myText}
-    </div>
-  );
+// function HelloWorld(props) {
+//   return (
+//     <div id="hello">
+//       <h3>{props.myText}</h3>
+//       {props.myText}
+//     </div>
+//   );
+// }
+
+
+// state: each component has its own state
+class HelloWorld extends React.Component {
+
+  state = {
+    show: true
+  }
+
+  toggleShow = () => {
+    this.setState({show: !this.state.show})
+  }
+
+  render() {
+    if (this.state.show) {
+      return (
+        <div id="hello">
+          <h3>{this.props.subtitle}</h3>
+          {this.props.myText}
+          <button onClick={this.toggleShow}>Toggle show</button>
+        </div>
+      );    
+    } else {
+      return <h1>
+        There are not elements
+        <button onClick={this.toggleShow}>
+          Toggle show
+        </button>
+      </h1>
+    }
+  }  
 }
 
 // class App extends React.Component {
