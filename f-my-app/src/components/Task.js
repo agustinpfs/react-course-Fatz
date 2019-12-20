318f01caec3410ca4ab55ab9841f98c57b807682
Agustin Palmieri <agujex@Agustins-MBP.utopia.net>
2019-12-19 21:52:58 -0300

    use ES6 destructuring

--- f-my-app/src/components/Task.js
+++ f-my-app/src/components/Task.js
@@ -3,11 +3,12 @@ import React, {Component} from 'react';
 
 class Task extends Component {
     render(){
+        const {task} = this.props;
         return <div>
-            {this.props.task.title} - 
-            {this.props.task.description} - 
-            {this.props.task.done} - 
-            {this.props.task.id}
+            {task.title} - 
+            {task.description} - 
+            {task.done} - 
+            {task.id}
             <input type ="checkbox" />
             <button>
                 X
