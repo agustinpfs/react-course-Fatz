a338d1e8e70714151e92f28c40f2172465f1ced3
Agustin Palmieri <agujex@Agustins-MBP.utopia.net>
2020-01-06 10:41:16 -0300

    Forms. Create TaskForm component

---
+++ f-my-app/src/components/TaskForm.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+export default class TaskForm extends Component {
+    render() {
+        return (
+            <form>
+                <input type = 'text' placeholder = 'write a task' />
+            </form>
+        )
+    }
+}
+
+
+// export default TaskForm;
