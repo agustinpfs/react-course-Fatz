a22ad14ef734e073240af202958cbbaab97546ed
Agustin Palmieri <agujex@Agustins-MBP.utopia.net>
2020-01-06 12:12:24 -0300

    remove console.log

--- f-my-app/src/components/TaskForm.js
+++ f-my-app/src/components/TaskForm.js
@@ -8,12 +8,10 @@ export default class TaskForm extends Component {
     }
 
     onSubmit = e => {
-        console.log(this.state)
         e.preventDefault();
     }
 
     onChange = e => {
-        console.log(e.target.name, e.target.value)
         this.setState({
             [e.target.name] : e.target.value
         })
