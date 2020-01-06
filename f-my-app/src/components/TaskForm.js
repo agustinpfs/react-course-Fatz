116f3a267e54c08a2fd8071f88edf8fedc3193fe
Agustin Palmieri <agujex@Agustins-MBP.utopia.net>
2020-01-06 11:02:08 -0300

    add textarea and submit and cancel refresh from submit behavior

--- f-my-app/src/components/TaskForm.js
+++ f-my-app/src/components/TaskForm.js
@@ -5,6 +5,8 @@ export default class TaskForm extends Component {
         return (
             <form>
                 <input type = 'text' placeholder = 'write a task' />
+                <br></br>
+                <textarea placeholder = 'write a description'></textarea>
             </form>
         )
     }
